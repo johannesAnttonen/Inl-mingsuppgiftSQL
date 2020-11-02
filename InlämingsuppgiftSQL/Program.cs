ae293a5ba8042eec1c1f3938e9a248e7dba4dc96
Johannes Anttonen <johannesAnttonen@users.noreply.github.com>
2020-11-02 18:18:38 +0100

    Add project files.

---
+++ InlämingsuppgiftSQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InlämingsuppgiftSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hej Björn Finns inte någonstans på PingPong för mig att lämna in denna uppgift
+        }
+    }
+}
